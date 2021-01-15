4c74d7b652159eb879db0efd79a4e24c6289d547
Rafael Caroprese <rafael.caroprese@rubix.nl>
2021-01-15 15:58:22 +0100

    12.3 - Grouping config values

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,28 @@ fn main() {
     // println!("{:?}", args);
     // println!("{:#?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    
     // Reading in a file
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
+    
+    let contents = fs::read_to_string(config.filename)
+    .expect("Something went wrong reading the file");
+    
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    
+    Config { query, filename }
 }
 
