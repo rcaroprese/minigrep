1773c51a1c1b875cf733acf6853efd183ec9e8cd
Rafael Caroprese <rafael.caroprese@rubix.nl>
2021-01-15 16:49:04 +0100

    12.4 - Writing a failing test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,22 @@
 use std::fs;
 use std::error::Error;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -23,7 +39,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Reading in a file
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n\n{}", contents);
-
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
 }
