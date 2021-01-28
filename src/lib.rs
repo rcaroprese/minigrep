2dc50311e120e93633be1a792a46156cf6f5e369
Rafael Caroprese <rafael.caroprese@rubix.nl>
2021-01-28 14:28:43 +0100

    Made code clearer with iterator adaptors

--- src/lib.rs
+++ src/lib.rs
@@ -99,15 +99,21 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
     
-    for line in contents.lines(){
-        if line.contains(query){
-            results.push(line);
-        }
-    }
+    // for line in contents.lines(){
+    //     if line.contains(query){
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    // Avoid having a mutable results vector by:
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
@@ -115,13 +121,19 @@ pub fn search_case_insensitive<'a>(
     contents: &'a str,
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results =  Vec::new();
+    // let mut results =  Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    // Avoid having a mutable results vector by:
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
